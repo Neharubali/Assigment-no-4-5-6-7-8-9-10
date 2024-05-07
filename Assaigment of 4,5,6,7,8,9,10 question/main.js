f96078875d7ebed20bc3034472c3bfca07edf02a
Neharubali <168661089+Neharubali@users.noreply.github.com>
2024-05-07 21:20:19 +0500

    git commit -m "first commit"

---
+++ Assaigment of 4,5,6,7,8,9,10 question/main.js
@@ -0,0 +1,41 @@
+// assigment no 04
+var famQoute = "A person who never made a mistake never tried anything new.";
+var author = "Albert Einstein";
+console.log("".concat(author, " once said, \"").concat(famQoute, "\""));
+//assigment no 05
+var famousPerson = "Albert Einstein";
+var message = "".concat(famousPerson, " once said, \"").concat(famQoute, "\"");
+console.log(message);
+//assigment no 06
+var nameWithWhiteSpace = "\t \n neha khan \t \n";
+console.log("name with white space:", nameWithWhiteSpace);
+var strippedName = nameWithWhiteSpace.trim();
+console.log("stripped Name:", strippedName);
+//assaigment no 07
+var additionResult = 5 + 3;
+console.log("Addition Result:", additionResult);
+var subtractionResult = 10 - 2;
+console.log("Subtraction Result:", subtractionResult);
+var multiplicationResult = 4 * 2;
+console.log("Multiplication Result:", multiplicationResult);
+var divisionResult = 16 / 2;
+console.log("Division Result:", divisionResult);
+//assigment no 08
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+//assigment no 09
+var favouriteResult = 9;
+var messageResult = "My favourite number: ".concat(favouriteResult);
+console.log(messageResult);
+// ctrl + question
+//assigment no 10
+//this programe adds two number and prints the result
+// define Two numbers
+var number1 = 10;
+var number2 = 20;
+// Add two numbers
+var result = number1 + number2;
+//print result
+console.log(result);
---
+++ Assaigment of 4,5,6,7,8,9,10 question/main.ts
@@ -0,0 +1,65 @@
+// assigment no 04
+let famQoute:string =  "A person who never made a mistake never tried anything new." 
+let author:string = "Albert Einstein";
+
+console.log(`${author} once said, "${famQoute}"`);
+
+//assigment no 05
+let famousPerson:string = "Albert Einstein";
+let message:string = `${famousPerson} once said, "${famQoute}"`;
+
+console.log(message);
+
+//assigment no 06
+let nameWithWhiteSpace:string ="\t \n neha khan \t \n";
+console.log("name with white space:",nameWithWhiteSpace);
+
+let strippedName:string = nameWithWhiteSpace.trim();
+console.log("stripped Name:", strippedName);
+
+//assaigment no 07
+let additionResult:number = 5+3;
+
+console.log("Addition Result:",additionResult);
+
+let subtractionResult:number =10-2;
+
+console.log("Subtraction Result:",subtractionResult);
+
+let multiplicationResult:number = 4*2;
+
+console.log("Multiplication Result:",multiplicationResult);
+
+let divisionResult:number = 16/2;
+
+console.log("Division Result:",divisionResult);
+
+//assigment no 08
+console.log(5+3);
+console.log(10-2);
+console.log(4*2);
+console.log(16/2);
+
+//assigment no 09
+let favouriteResult:number = 9;
+
+let messageResult:string =`My favourite number: ${favouriteResult}`
+
+console.log(messageResult);
+
+// ctrl + question
+
+//assigment no 10
+
+//this programe adds two number and prints the result
+
+// define Two numbers
+let number1:number =10;
+let number2:number =20;
+// Add two numbers
+let result:number = number1 + number2;
+//print result
+console.log(result);
+
+
+
